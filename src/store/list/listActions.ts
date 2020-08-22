1f704592a4da28e89ee4a976647becc3f825f3b8
mvximenko <mr.mxmnk@gmail.com>
2020-08-22 10:25:45 +0900

    Update listActions

--- src/store/list/listActions.ts
+++ src/store/list/listActions.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { API_URL, API_KEY } from '../../config';
-import { SET_LIST, FetchListAction } from './listTypes';
+import { SET_LIST, FetchListAction, Titles } from './listTypes';
 
 const endpoints = [
   { id: 49, network: 'HBO' },
@@ -19,13 +19,20 @@ export default function useFetchLists(
 ): void {
   useEffect(() => {
     endpoints.forEach(({ id, network }) => {
-      fetch(
-        `${API_URL}discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_networks=${id}`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch({ type: SET_LIST, network, titles: data.results });
-        });
+      const key: string = network;
+      if (sessionStorage.getItem(key)) {
+        const data = JSON.parse(sessionStorage.getItem(key) as string);
+        dispatch({ type: SET_LIST, network, titles: data.results });
+      } else {
+        fetch(
+          `${API_URL}discover/tv?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&with_networks=${id}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            dispatch({ type: SET_LIST, network, titles: data.results });
+            sessionStorage.setItem(key, JSON.stringify(data));
+          });
+      }
     });
   }, [dispatch]);
 }
